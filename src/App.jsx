9e3cd60e3ff639dba3594b0fe821f78b8bdf4f4c
David <dave.obat@student.moringashool.com>
2023-12-13 14:56:02 +0300

    Basic nav bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 
+import { Route, Routes } from 'react-router'
 import './App.css'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import { Container } from '@chakra-ui/react'
+import Navbar from './components/Navbar'
 
 function App() {
   
-
   return (
     <div>
-      Home
+
+    <Navbar />
+    <Container>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='collection' element={<Collection />}></Route>
+    </Routes>
+    </Container>
+      
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+import { Box, Flex, HStack } from '@chakra-ui/react';
+import { AtSignIcon, TriangleDownIcon } from '@chakra-ui/icons';
+
+
+function Navbar() {
+  return (
+    <Box px={5}>
+        <Flex justifyContent={'space-between'}>
+            <HStack spacing={8} h={14}>
+                <TriangleDownIcon w={8} h={8} color={'burlywood'}/>
+                <HStack spacing={4}>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/collection'}>Collection</Link>
+                </HStack>
+            </HStack>
+            <Flex py={4}>
+              <AtSignIcon w={8} h={8} color={'burlywood'}/> 
+            </Flex>
+        </Flex>
+    </Box>
+  )
+}
+
+export default Navbar
